a6206f6558680464ac0be037a282ba40452387e8
Arly <pechnikov.danya08@gmail.com>
2026-09-06 18:57:48 +0300

    Добавьте файлы проекта.

---
+++ Librery sistem/Second_PR.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void Librery_menu()
+{
+	std::cout << std::endl;
+	std::cout << "Что хотите выбрать?" << std::endl;
+	std::cout << "1. Показать все книги" << std::endl;
+	std::cout << "2. Добавить книгу" << std::endl;
+	std::cout << "3. Удалить книгу" << std::endl;
+	std::cout << "4. Найти книгу" << std::endl;
+	std::cout << "5. Показать всех читателей" << std::endl;
+	std::cout << "6. Добавить читателя" << std::endl;
+	std::cout << "7. Удалить читателя" << std::endl;
+	std::cout << "8. Найти читателя" << std::endl;
+	std::cout << "9. Выдать книгу" << std::endl;
+	std::cout << "10. Вернуть книгу" << std::endl;
+	std::cout << "11. Показать выданные книги" << std::endl;
+	std::cout << "12. Сохранить данные" << std::endl;
+	std::cout << "13. Загрузить данные" << std::endl;
+	std::cout << "0. Выход" << std::endl;
+	std::cout << std::endl;
+}
